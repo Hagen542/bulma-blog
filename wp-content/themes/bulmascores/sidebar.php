8c464f8e63e23aaa71f62941b03be6da1ad7d261
Alexander Hagen <alexander.hagen@adera.no>
2019-01-30 14:01:00 +0100

    Lang stuff. Added box to sidebar. commented out some media query stuff...

--- wp-content/themes/bulmascores/sidebar.php
+++ wp-content/themes/bulmascores/sidebar.php
@@ -13,5 +13,10 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area menu">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+    <div class="box">
+        <h2 class="title">Sign up to our newsletter</h2>
+        <p class="has-margin-b-5">Get the most awesome content delivered straight to your inbox!</p>
+        <button class="button is-action">Sign up</button>
+    </div>
+    <?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
