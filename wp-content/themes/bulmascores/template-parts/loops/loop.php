60df0944edd4ea65410f6cb0b301c3c24c0b870d
Alexander Hagen <alexander.hagen@adera.no>
2019-01-31 08:49:36 +0100

    Changed button for read more. Some formatting.

--- wp-content/themes/bulmascores/template-parts/loops/loop.php
+++ wp-content/themes/bulmascores/template-parts/loops/loop.php
@@ -8,36 +8,35 @@
 */
 
 ?>
+<article class="box" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header>
+        <?php the_title( '<h2 class="title is-size-1"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
 
-	<header>
-		<?php the_title( '<h2 class="title is-marginless"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+        <?php if ( 'post' === get_post_type() ) : ?>
+            <div class="post-meta">
+<!--				<p>-->
+<!--					--><?php //esc_html_e( 'Last modified&nbsp;', 'bulmascores' )?><!--<time>--><?php //the_modified_date( 'j/m/Y' ); ?><!--</time>-->
+<!--					--><?php //esc_html_e( 'by&nbsp;', 'bulmascores' ) . the_author_posts_link();?>
+<!--					--><?php //bulmascores_post_meta( 'categories' ); ?>
+<!--				</p>-->
+            </div><!-- .post-meta -->
+        <?php endif; ?>
+    </header>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-			<div class="post-meta">
-				<p>
-					<?php esc_html_e( 'Last modified&nbsp;', 'bulmascores' )?><time><?php the_modified_date( 'j/m/Y' ); ?></time>
-					<?php esc_html_e( 'by&nbsp;', 'bulmascores' ) . the_author_posts_link();?>
-					<?php bulmascores_post_meta( 'categories' ); ?>
-				</p>
-			</div><!-- .post-meta -->
-		<?php endif; ?>
-	</header>
+    <?php
+        the_post_thumbnail( 'bulmascores_thumbnail', array(
+            'class' => 'has-margin-b-5 is-fullwidth',
+            'alt' => get_the_title(),
+            'title' => get_the_title(),
+        ) ); ?>
 
-	<?php
-		the_post_thumbnail( 'bulmascores_thumbnail', array(
-			//'class' => 'attachment-post-thumbnail my-custom-class',
-			'alt' => get_the_title(),
-			'title' => get_the_title(),
-		) ); ?>
+    <div class="post-content has-margin-b-4">
+        <?php the_excerpt(); ?>
+    </div><!-- .post-content -->
 
-	<div class="post-content">
-		<?php the_excerpt(); ?>
-	</div><!-- .post-content -->
+    <footer>
+        <a href="<?php the_permalink(); ?>"> Read more <i class="far fa-chevron-right"></i></a>
+    </footer>
 
-	<footer>
-		<a href="<?php the_permalink(); ?>"><span class="button is-dark read-more"><?php esc_html_e( 'Read More', 'bulmascores' ); ?></span></a>
-	</footer>
-	<hr>
 </article><!-- #post-<?php the_ID(); ?> -->
