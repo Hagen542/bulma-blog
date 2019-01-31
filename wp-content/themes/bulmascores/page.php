de55a47f6f7722b4a43145e96fdde879bd4d481f
Alexander Hagen <alexander.hagen@adera.no>
2019-01-31 08:42:22 +0100

    Removed comments.

--- wp-content/themes/bulmascores/page.php
+++ wp-content/themes/bulmascores/page.php
@@ -14,28 +14,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="container">
+<div id="primary" class="content-area">
+    <main id="main" class="container">
 
-			<div class="columns is-centered">
-				<div class="column is-8">
-					<?php
-					while ( have_posts() ) : the_post();
+        <div class="columns is-centered">
+            <div class="column is-8">
+                <?php
+                while ( have_posts() ) : the_post();
 
-						get_template_part( 'template-parts/loops/loop', 'page' );
+                    get_template_part( 'template-parts/loops/loop', 'page' );
 
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
+                    // If comments are open or we have at least one comment, load up the comment template.
+//                    if ( comments_open() || get_comments_number() ) :
+//                        comments_template();
+//                    endif;
 
-					endwhile; // End of the loop.
-					?>
+                endwhile; // End of the loop.
+                ?>
 
-				</div><!-- .columns is-8 -->
-			</div><!-- .columns -->
+            </div><!-- .columns is-8 -->
+        </div><!-- .columns -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
--- wp-content/themes/bulmascores/single.php
+++ wp-content/themes/bulmascores/single.php
@@ -13,21 +13,24 @@ get_header(); ?>
 	<main id="main" class="container">
 
 		<div class="columns is-centered">
-			<div class="column is-8">
+			<div class="column is-9">
 				<?php
 				while ( have_posts() ) : the_post();
 
 					get_template_part( 'template-parts/loops/loop', 'single' );
 
 					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+//					if ( comments_open() || get_comments_number() ) :
+//						comments_template();
+//					endif;
 
 				endwhile; // End of the loop.
 				?>
 
-			</div><!-- .columns is-8 -->
+			</div><!-- .columns is-9 -->
+            <div class="column is-3">
+                <?php get_sidebar(); ?>
+            </div><!-- .columns is-3 -->
 		</div><!-- .columns -->
 
 	</main><!-- #main -->
